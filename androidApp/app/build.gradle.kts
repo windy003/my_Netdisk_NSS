e2d55efe81334fe8de86945768b7199d1356e418
windy003 <928920471@qq.com>
2026-01-03 07:59:05 +0800

    调整了代码

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "2026/1/2-1"
+        versionName = "2026/1/3-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
