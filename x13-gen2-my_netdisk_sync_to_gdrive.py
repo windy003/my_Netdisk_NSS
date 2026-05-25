26a1934f178f5f483ccc4f7a7c307fcc646c69d7
windy003 <928920471@qq.com>
2026-05-25 10:04:59 +0800

    修改

--- x13-gen2-my_netdisk_sync_to_gdrive.py
+++ x13-gen2-my_netdisk_sync_to_gdrive.py
@@ -27,7 +27,7 @@ source_path = r"D:\files\myNetdisk"
 
     
 # 目标路径
-destination_path = 'gdrive:/sync/x13y/my_netdisk'
+destination_path = 'gdrive:/sync/x13-gen2/my_netdisk'
 
 def dedupe_gdrive():
     """
